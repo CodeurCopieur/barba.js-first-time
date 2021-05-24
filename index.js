203b3dcfd88eb6169f20da62b23ea0aac0159034
codwerk <louis_widdy@live.fr>
2021-05-24 20:21:12 +0200

    second aniamtion

--- index.js
+++ index.js
@@ -1,4 +1,5 @@
 const wipe = document.querySelector('.wipe-transition');
+const allLayer = document.querySelectorAll('.layer');
 
 const tl = new TimelineMax();
 
@@ -18,18 +19,22 @@ barba.init({
 
         const done = this.async();
 
-        tl.to(wipe, {left: '0%', ease: "power2.out", duration:.5});
+        //tl.to(wipe, {left: '0%', ease: "power2.out", duration:.5});
 
-        await delay(1000);
+        tl
+        .to(allLayer, {height: '100%', stagger: 0.15})
+
+        await delay(1500);
         done();
       },
       enter(){
 
         tl
+        .to(allLayer, {height: '0%', stagger: 0.15})
+
+        /*tl
         .to(wipe, {left: '100%', ease: "power2.out", duration:.5})
-        .set(wipe, {left: '-100%'})
-
-
+        .set(wipe, {left: '-100%'})*/
       }
     }
   ]
